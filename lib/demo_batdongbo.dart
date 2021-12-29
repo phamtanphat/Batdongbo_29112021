d9acadc3f72f8ff5797b01760be1464a5937c157
phamtanphat <phatdroid94@gmail.com>
2021-12-29 20:29:31 +0700

    async await

--- lib/demo_batdongbo.dart
+++ lib/demo_batdongbo.dart
@@ -1,18 +1,13 @@
 import 'dart:async';
 
 void main(){
-  Future<int> data = tinhTong(5, 10);
-  data.then((value) => print(value));
+  // Future<int> data = tinhTong(5, 10);
+  // data.then((value) => print(value));
+  doSthing();
 }
 
-Future<int> tinhTong(int a , int b){
-  Completer<int> completer = Completer();
-  Future.delayed(Duration(seconds: 2),(){
-     Future.delayed(Duration(seconds: 1),(){
-       int c = a + b;
-       completer.complete(c);
-       return a;
-     });
-  });
-  return completer.future;
+void doSthing() async{
+  var data = await Future.delayed(Duration(seconds: 2), () => 1);
+  print("alo");
+  print(data);
 }
